da78ceca18bfc6eef92e3ea4fc2bdefa717e09c5
ApeSkull13 <noobskull13@gmail.com>
2021-02-09 12:21:08 -0800

    Testing.

---
+++ src/BotTest.java
@@ -0,0 +1,10 @@
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+@ScriptManifest(author = "TPSKNNP", name = "A Test Script", info = "An empty script :(", version = 0.1, logo = "https://i.imgur.com/Jqkusgs.png")
+public final class BotTest extends Script {
+    @Override
+    public final int onLoop() throws InterruptedException {
+        return 0;
+    }
+}
